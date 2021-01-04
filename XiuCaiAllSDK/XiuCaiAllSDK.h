acc86b5c959af5b8bcb4390ae7ece0ea2494fb7a
kent <chengyakun11@163.com>
2021-01-04 16:04:45 +0800

    Initial Commit

---
+++ XiuCaiAllSDK/XiuCaiAllSDK.h
@@ -0,0 +1,18 @@
+//
+//  XiuCaiAllSDK.h
+//  XiuCaiAllSDK
+//
+//  Created by kent on 2021/1/4.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for XiuCaiAllSDK.
+FOUNDATION_EXPORT double XiuCaiAllSDKVersionNumber;
+
+//! Project version string for XiuCaiAllSDK.
+FOUNDATION_EXPORT const unsigned char XiuCaiAllSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <XiuCaiAllSDK/PublicHeader.h>
+
+
